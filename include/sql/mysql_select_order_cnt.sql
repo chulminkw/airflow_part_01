e612ae366131e084fc51df05bc3e213b5a3dc58b
chulminkw <55954384+chulminkw@users.noreply.github.com>
2026-03-02 09:22:56 +0900

    Delete include directory

--- include/sql/mysql_select_order_cnt.sql
+++
@@ -1,6 +0,0 @@
--- 주석은 sql 주석인 -- 또는 /* */
--- 여러 sql 문장들의 경우 개별 sql은 ; 반드시 적용.
-SELECT count(*) as cnt
-FROM orders
-WHERE order_dt >= '{{ data_interval_start }}'
-AND order_dt <  '{{ data_interval_end }}';
